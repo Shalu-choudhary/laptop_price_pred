a817b91d19b649c3920f24e463b505a41030d4f2
Shalu-choudhary <choudharyshalu598@gmail.com>
2024-08-09 21:27:55 +0530

    laptoppricepred

--- app.py
+++ app.py
@@ -11,35 +11,37 @@ def home():
 @app.route('/prediction', methods=['GET','POST'])
 
 def prediction():
-    if request.method=='POST':
-        brand=request.form['brand']
-        spec_rating=int(request.form['spec_rating'])
-        processor=request.form['processor']
-        Ram=int(request.form['Ram'])
-        Rom=int(request.form['ROM'])
-        ROM_type=request.form['ROM_type']
-        display_size=int(request.form['display_size'])
-        resolution_width=int(request.form['resolution_width'])
-        resolution_height=int(request.form['resolution_height'])
-        warranty=int(request.form['warranty'])
-        os=request.form['OS']
-        CPU_Name=request.form['CPU Name']
-        GPU_brand=request.form['GPU_brand']
-        
-        data=[[brand, spec_rating, processor, Ram, Rom, ROM_type,
-       display_size, resolution_width, resolution_height, os,
-       warranty, CPU_Name, GPU_brand]]
-        prediction=model.predict(data)[0]
-        print(prediction)
-        return str(round(prediction[0],2))
+   
+    if request.method == 'POST':
+        try:
+            # Retrieve form data
+            brand = request.form.get('brand', '')
+            spec_rating = int(request.form.get('spec_rating', 0))
+            processor = request.form.get('processor', '')
+            Ram = int(request.form.get('Ram', 0))
+            Rom = int(request.form.get('ROM', 0))  # Make sure 'ROM' exists in the form
+            ROM_type = request.form.get('ROM_type', '')
+            display_size = int(request.form.get('display_size', 0))
+            resolution_width = int(request.form.get('resolution_width', 0))
+            resolution_height = int(request.form.get('resolution_height', 0))
+            warranty = int(request.form.get('warranty', 0))
+            os = request.form.get('OS', '')
+            CPU_Name = request.form.get('CPU Name', '')
+            GPU_brand = request.form.get('GPU_brand', '')
 
+            # Prepare data for prediction
+            data = [[brand, spec_rating, processor, Ram, Rom, ROM_type,
+                    display_size, resolution_width, resolution_height, os,
+                    warranty, CPU_Name, GPU_brand]]
 
-#'brand', 'spec_rating', 'processor', 'Ram', 'ROM', 'ROM_type',
-       #'display_size', 'resolution_width', 'resolution_height', 'OS',
-       #'warranty', 'CPU Name', 'GPU_brand'
+            # Predict
+            prediction = model.predict(data)[0]
+            return str(round(prediction, 2))
 
+        except Exception as e:
+            return str(e)  # Return the error message
 
-
+    return render_template('home.html')
 
 if __name__=="__main__":
     app.run(debug=True)
